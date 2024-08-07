ad0d1154648521ae56923397fda317855074b478
William <w.illiam@qq.com>
2024-08-07 03:15:18 +0000

    git命令行支持中文

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,2 +1,17 @@
-sudo service ssh start &&
-/usr/bin/code-server --bind-addr=0.0.0.0:8080 /app/backend/
+# sudo /usr/sbin/sshd -D > /dev/null 2>&1 &
+if [ ! -f /var/www/.bashrc ]; then
+    # bash 支持常用别名
+    sed -i 's/# *alias /alias /g' ~/.bashrc && \
+    source ~/.bashrc
+
+    # bash 支持中文（修改后需要退出后重新进）
+    echo 'export LANG=C.UTF-8' >> ~/.bashrc
+
+    # git 支持中文显示
+    git config --global core.fileMode false
+    git config --global core.quotepath off
+    git config --global core.pager more
+fi
+
+sudo service ssh start && \
+    /usr/bin/code-server --bind-addr=0.0.0.0:8080 /app/backend/
